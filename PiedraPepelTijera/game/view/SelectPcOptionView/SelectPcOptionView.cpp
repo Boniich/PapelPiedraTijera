cbe0ada60c181bd97ec9422e67deab828408f5a9
Boniich <ezequieldbo25@gmail.com>
2024-11-25 07:43:12 -0300

    feat: show wich option is seleted by pc

--- PiedraPepelTijera/game/view/SelectPcOptionView/SelectPcOptionView.cpp
+++ PiedraPepelTijera/game/view/SelectPcOptionView/SelectPcOptionView.cpp
@@ -1,12 +1,15 @@
 #include "SelectPcOptionView.h"
 #include <iostream>
 #include "../../model/SelectPcOption/SelectPcOption.h"
+#include "../../helpers/ShowElementsName/ShowElementsName.h"
 
 int selectPcOptionView() {
 
 	int seleted = selectPcOption();
 
-	std::cout << "La opcion seleccionada por la PC es: " << seleted << std::endl;
+	std::string name = showElementName(seleted);
+
+	std::cout << "La opcion seleccionada por la PC es: " << name << std::endl;
 
 	return seleted;
 }
