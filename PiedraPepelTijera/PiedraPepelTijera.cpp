f306bfa15151451887e8f96138516445cdc2115f
Boniich <ezequieldbo25@gmail.com>
2024-11-26 08:10:52 -0300

    feat: add at main the function that determine winner

--- PiedraPepelTijera/PiedraPepelTijera.cpp
+++ PiedraPepelTijera/PiedraPepelTijera.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include "game/view/SelectPlayerOptionView/SelectPlayerOptionView.h"
 #include "game/view/SelectPcOptionView/SelectPcOptionView.h"
+#include "game/view/DetermineWinnerView/DetermineWinnerView.h"
 
 int main()
 {
 	int playerSelection = selectPlayerOption();
 	int pcSelection = selectPcOptionView();
 
+	determineWinnerView(playerSelection, pcSelection);
+	
+	return 0;
 }
 
