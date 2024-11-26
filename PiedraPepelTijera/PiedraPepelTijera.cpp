37af7be1fad30e54764797c9a4b6fe374957a5b7
Boniich <ezequieldbo25@gmail.com>
2024-11-26 08:56:01 -0300

    feat: update main with the new functions and procedurals

--- PiedraPepelTijera/PiedraPepelTijera.cpp
+++ PiedraPepelTijera/PiedraPepelTijera.cpp
@@ -2,13 +2,14 @@
 #include "game/view/SelectPlayerOptionView/SelectPlayerOptionView.h"
 #include "game/view/SelectPcOptionView/SelectPcOptionView.h"
 #include "game/view/DetermineRoundWinnerView/DetermineRoundWinnerView.h"
+#include "game/view/DetermineGameWinnerView/DetermineGameWinnerView.h"
 
 int main()
 {
 	bool isRoundEnd = false;
 	int roundNumber = 1;
-	int roundWonByPlayer = 0;
-	int roundWonByPc = 0;
+	unsigned int roundWonByPlayer = 0;
+	unsigned int roundWonByPc = 0;
 
 	do
 	{
@@ -18,35 +19,12 @@ int main()
 		std::cout << "-------------------------------------------------------------" << std::endl;
 		std::cout << "" << std::endl;
 
-
-
 		int playerSelection = selectPlayerOption();
 		int pcSelection = selectPcOptionView();
 
 		int winner = determineRoundWinnerView(playerSelection, pcSelection);
 
-
-
-		if (winner == 2) {
-			roundWonByPlayer++;
-			if (roundWonByPlayer >= 2) {
-				isRoundEnd = true;
-				std::cout << "Ha ganado el jugador" << std::endl;
-			}
-		}
-
-		if (winner == 3) {
-			roundWonByPc++;
-			if (roundWonByPc >= 2) {
-				isRoundEnd = true;
-				std::cout << "Ha ganado la PC" << std::endl;
-			}
-		}
-
-		std::cout << "-------------------------------------------------------------" << std::endl;
-		std::cout << "Rondas ganadas para el jugador: " << roundWonByPlayer << std::endl;
-		std::cout << "Rondas ganadas para el PC: " << roundWonByPc << std::endl;
-		std::cout << "-------------------------------------------------------------" << std::endl;
+		isRoundEnd = determineGameWinnerView(winner, roundWonByPlayer, roundWonByPc);
 
 		roundNumber++;
 
